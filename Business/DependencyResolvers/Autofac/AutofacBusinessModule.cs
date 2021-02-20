8fe8e27bdbe6acdde51efd62d0e18d0f1ee5223d
fatih karayel <fkarayel@live.com>
2021-02-20 17:35:35 +0300

    AutofacBusinessModule düzeltme 20 Şub 2021

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -16,8 +16,8 @@ namespace Business.DependencyResolvers.Autofac
     {      
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<ProductManager>().As<IProductService>();
-            builder.RegisterType<EfProductDal>().As<IProductDal>();
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
 
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
 
